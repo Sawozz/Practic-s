085f63de5b702efbd4ed63cfe8134ecfd3c4520b
Sawozz <cavva45@gmail.com>
2024-02-16 13:52:50 +0300

    Create 3.cs

---
+++ Practic's/function/3.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Function3
+{
+    static void Main()
+    {
+        Console.WriteLine("Напишите");
+        string word = Console.ReadLine();
+        Console.WriteLine("Длинна строки");
+        void Lenght()
+        {
+            Console.WriteLine(word.Length);
+        }
+        Lenght();
+    }
+}
